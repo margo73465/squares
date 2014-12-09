7bbbe5b70c6963774006a47a34445229db6c1d73
Margo73465 <margo.lee.smith@gmail.com>
2014-12-09 12:38:55 -0500

    fades are working -- switched to fade functions that use a closure, also got rid of call to draw_random_square inside the remove_square function, it seemed to be the culprit for the flickering

--- squares.js
+++ squares.js
@@ -42,7 +42,7 @@ function draw_square(x, y, id) {
 
 	// Outer square
 	var g = document.createElementNS("http://www.w3.org/2000/svg", "g");
-	g.setAttribute("id", id);
+	g.setAttribute("id", "grid_" + id);
 	g.setAttribute("opacity", 0.0);
 	svg.appendChild(g);
 
@@ -64,84 +64,76 @@ function draw_square(x, y, id) {
 	inner_square.style.fill = "black";
 	g.appendChild(inner_square);
 
-	start_fade_in(g);
+	// start_fade_in(g);
+	fade_in(g);
 
 }
 
 function draw_random_square(grid) {
+	if (no_more_squares) {
+		return;
+	}
 
-	//var grid_point = grid[Math.floor(Math.random() * grid.length)];
-	grid_point = grid[0];
+	var grid_point = grid[Math.floor(Math.random() * grid.length)];
+	// var grid_point = grid[Math.floor(Math.random() * 2)];
+	// grid_point = grid[0];
 
 	if (grid_point.has_square === false) {
 	
+		console.log("making new square");
 		grid_point.has_square = true;
 		grid[grid_point.id] = grid_point;
+		
 		draw_square(grid_point.x, grid_point.y, grid_point.id);
 	
 	} else {
 
-		var child = svg.getElementById(grid_point.id);
-		start_fade_out(child);
+		console.log("removing old square");
+		var child = svg.getElementById("grid_" + grid_point.id);
+		fade_out(child);
 		setTimeout(remove_square, 1000, child, grid_point);
 
 	}
 }
 
 function remove_square(child, grid_point) {
-	console.log(svg);
-	console.log(child);
+	no_more_squares = false;
 	svg.removeChild(child);
 	grid_point.has_square = false;
 	grid[grid_point.id] = grid_point;
-	draw_random_square(grid);
+
+	// draw_random_square(grid);
 }
 
-function start_fade_in(svg_element) {
-	var opacity = svg_element.getAttribute("opacity");
-	
-	var fade = setInterval(function() {
-			if (svg_element.getAttribute("opacity") >= 0.9) {
-				clearInterval(fade);
-			}
-			fade_in(svg_element);
-		}, 10);
+function fade_in (svg_element) {
+	var level = 0.1;
+	var step = function () {
+		if (level < 1.0) {
+			level += 0.1;
+			svg_element.setAttribute("opacity", level);
+			// console.log(level);
+			setTimeout(step, 100);
+		}
+	};
+	setTimeout(step, 100);
 }
 
-function fade_in(svg_element, fade) {
-	console.log("opacity up!");
-	var opacity = svg_element.getAttribute("opacity");
-	var new_opacity = Number(opacity) + 0.1;
-	svg_element.setAttribute("opacity", new_opacity);
-}
+var no_more_squares = false;
 
-function start_fade_out(svg_element) {
-	var fade = setInterval(function() {
-			if (svg_element.getAttribute("opacity") <= 0.1) {
-				clearInterval(fade);
-			}
-			fade_out(svg_element);
-		}, 10);
+function fade_out (svg_element) {
+	var level = svg_element.getAttribute("opacity");
+	no_more_squares = true;
+	var step = function () {
+		if (level > 0.0) {
+			level -= 0.1;
+			svg_element.setAttribute("opacity", level);
+			// console.log(level);
+			setTimeout(step, 100);
+		}
+	};
+	setTimeout(step, 100);
 }
-
-function fade_out(svg_element, fade) {
-	console.log("opacity down!")
-	var opacity = svg_element.getAttribute("opacity");	
-	var new_opacity = Number(opacity) - 0.1;
-	svg_element.setAttribute("opacity", new_opacity);
-}
-
-// draw_square(center[0] - square_dim/2, center[1] - square_dim/2, "MIDDLE");
-// var square = svg.getElementById("MIDDLE");
-// setTimeout(start_fade_out, 1000, square);
 
 draw_random_square(grid);
-setTimeout(draw_random_square, 1000, grid);
-setTimeout(draw_random_square, 1000, grid);
-setTimeout(draw_random_square, 1000, grid);
-setTimeout(draw_random_square, 1000, grid);
-setTimeout(draw_random_square, 1000, grid);
-setTimeout(draw_random_square, 1000, grid);
-setTimeout(draw_random_square, 1000, grid);
 
-//square_maker = setInterval(draw_random_square, 1002, grid);
+square_maker = setInterval(draw_random_square, 1500, grid);
