239aa00530b45968143fed07bf04288e73c0770f
Margo <margo.lee.smith@gmail.com>
2020-06-02 16:09:35 -0700

    move from js animations to css animations

--- squares.js
+++ squares.js
@@ -3,8 +3,7 @@ var width = window.innerWidth;
 var grid_dim = Math.floor(width / 80);
 var square_dim = 7 * grid_dim;
 var grid_height = Math.ceil(height / (square_dim + grid_dim)) * 4;
-var grid_width = Math.ceil(width / (square_dim + grid_dim)) * 4;
-
+var grid_width = Math.ceil(width / (square_dim + grid_dim)) * 4; 
 var square_maker;
 var speed = 600; // Must be greater than 200 for fades to work
 var fade_duration = speed - 200;
@@ -34,6 +33,7 @@ function create_grid() {
           x: i * (2 * grid_dim) + grid_dim - square_dim / 2,
           y: j * (2 * grid_dim) + grid_dim - square_dim / 2
         };
+        draw_square(point);
         grid.push(point);
       }
     }
@@ -63,72 +63,28 @@ function draw_random_square(grid) {
 
   if (grid[id].has_square === false) {
     grid[id].has_square = true;
-    draw_square(grid[id]);
+    grid[id].svg.style.opacity = 1;
     total_squares += 1;
   } else {
-    var child = svg.getElementById("square_" + id);
-    fade_out(child, fade_duration);
-    setTimeout(remove_square, fade_duration, child, id);
+    grid[id].has_square = false;
+    grid[id].svg.style.opacity = 0;
     total_squares -= 1;
   }
 }
 
 
 function draw_square(grid_point) {
-  // Outer square
-  var g = document.createElementNS("http://www.w3.org/2000/svg", "g");
-  g.setAttribute("id", "square_" + grid_point.id);
-  g.setAttribute("opacity", 0.0);
-  svg.appendChild(g);
-
-  var outer_square = document.createElementNS("http://www.w3.org/2000/svg", 'rect');
-  outer_square.setAttribute("x", grid_point.x);
-  outer_square.setAttribute("y", grid_point.y);
-  outer_square.setAttribute("width", square_dim - grid_dim);
-  outer_square.setAttribute("height", square_dim - grid_dim);
-  outer_square.style.stroke = "white";
-  outer_square.style["stroke-width"] = grid_dim;
-  outer_square.style.fill = "none";
-  g.appendChild(outer_square);
-
-  fade_in(g, fade_duration);
-}
-
-
-function remove_square(child, id) {
-  svg.removeChild(child);
-  grid[id].has_square = false;
-}
-
-
-function fade_in (svg_element, duration) {
-  var opacity = 0.0;
-  var step_time = duration / 10;
-
-  var step = function () {
-    if (opacity < 1.0) {
-      opacity += 0.1;
-      svg_element.setAttribute("opacity", opacity);
-      setTimeout(step, step_time);
-    }
-  };
-
-  setTimeout(step, step_time);
-}
-
-
-function fade_out (svg_element, duration) {
-  var opacity = 1.0;
-  var step_time = duration / 10;
-
-  var step = function () {
-    if (opacity > 0.0) {
-      opacity -= 0.1;
-      svg_element.setAttribute("opacity", opacity);
-      setTimeout(step, step_time);
-    }
-  };
-
-  setTimeout(step, step_time);
+  var square = document.createElementNS("http://www.w3.org/2000/svg", 'rect');
+  square.setAttribute("x", grid_point.x);
+  square.setAttribute("y", grid_point.y);
+  square.setAttribute("width", square_dim - grid_dim);
+  square.setAttribute("height", square_dim - grid_dim);
+  square.setAttribute("class", "square");
+  square.style.stroke = "white";
+  square.style["stroke-width"] = grid_dim;
+  square.style.fill = "none";
+  square.style.opacity = 0;
+  svg.appendChild(square);
+  grid_point.svg = square;
 }
 
